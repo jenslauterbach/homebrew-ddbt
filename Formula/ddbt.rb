43c55e054ab20fcd904de1e626ac00eb2be3c2b8
goreleaserbot <goreleaser@carlosbecker.com>
2021-03-14 10:30:42 +0100

    Brew formula update for ddbt version v0.1.1

--- Formula/ddbt.rb
+++ Formula/ddbt.rb
@@ -5,24 +5,24 @@
 class Ddbt < Formula
   desc "ddbt is a simple command line tool that does one job and one job only: delete all items in a AWS DynamoDB table."
   homepage "https://github.com/jenslauterbach/ddbt/"
-  version "0.1.0"
+  version "0.1.1"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/jenslauterbach/ddbt/releases/download/v0.1.0/ddbt_0.1.0_Darwin_x86_64.tar.gz", :using => CurlDownloadStrategy
-    sha256 "df0de9a049a4a79ff7b11db1689dce2da9a7a8a124c3df1aa78c9d7a7caf4da9"
+    url "https://github.com/jenslauterbach/ddbt/releases/download/v0.1.1/ddbt_0.1.1_Darwin_x86_64.tar.gz", :using => CurlDownloadStrategy
+    sha256 "faa15016a7389511a7a75b79cc9966c589c4ee0936704cd9c4496616b7e37796"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/jenslauterbach/ddbt/releases/download/v0.1.0/ddbt_0.1.0_Darwin_arm64.tar.gz", :using => CurlDownloadStrategy
-    sha256 "728459e3e6b9f731bf5620e0325df419b5190ddea4111f35e89d051711cb57ec"
+    url "https://github.com/jenslauterbach/ddbt/releases/download/v0.1.1/ddbt_0.1.1_Darwin_arm64.tar.gz", :using => CurlDownloadStrategy
+    sha256 "83515702fdcc3f3839bf1ab79150d60456ce1600afe5618a9473f3ada094d41f"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/jenslauterbach/ddbt/releases/download/v0.1.0/ddbt_0.1.0_Linux_x86_64.tar.gz", :using => CurlDownloadStrategy
-    sha256 "88b293c949004eb492dea6fd81ba8fbad5a49934d254255909bc686e7ec64553"
+    url "https://github.com/jenslauterbach/ddbt/releases/download/v0.1.1/ddbt_0.1.1_Linux_x86_64.tar.gz", :using => CurlDownloadStrategy
+    sha256 "2745a2f219cb721c998ce657ee9478a5179ee298e1423519973d84fd421e9e21"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/jenslauterbach/ddbt/releases/download/v0.1.0/ddbt_0.1.0_Linux_arm64.tar.gz", :using => CurlDownloadStrategy
-    sha256 "de430a40a6ca46359cf32cc89d42d69456a2b704b6f4971983aad8c1f81fda54"
+    url "https://github.com/jenslauterbach/ddbt/releases/download/v0.1.1/ddbt_0.1.1_Linux_arm64.tar.gz", :using => CurlDownloadStrategy
+    sha256 "a1178a4f93829390fc15ce4485db66d186e67f69e898635731b60f21101e5831"
   end
 
   def install
